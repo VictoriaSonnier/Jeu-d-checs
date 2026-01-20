28c378043530180b1b77268ddac833b362f72533
Dupont-Barnich Isaura <isaura.dupontbarnich@gmail.com>
2026-01-20 14:13:22 +0100

    Tracer le damier

--- echec.py
+++ echec.py
@@ -1,9 +1,16 @@
 import pyxel
 from pathlib import Path
-WIDTH=30
-HEIGHT=30
+WIDTH=8
+HEIGHT=8
 BLACK=0
 WHITE=7
+#o, crée le damier
+damier=[]
+for i in range(8):
+    for j in range(8):
+        if (i+j)%2==0:
+            damier.append((i,j))
+
 def display(color,pixels=None):
     if pixels is None:
         pyxel.cls(color)
@@ -11,9 +18,10 @@ def display(color,pixels=None):
     else:
         for x,y in pixels:
             pyxel.pset(x,y, color)
-def draw_maze(maze):
+def draw_damier(maze):
     display(BLACK)
-    display(WHITE,random_maze)
+    display(WHITE,damier)
+#on trace le damier
 pyxel.init(WIDTH,HEIGHT)
-draw_maze(random_maze)
+draw_damier(damier)
 pyxel.show()
